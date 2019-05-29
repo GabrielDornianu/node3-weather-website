3798e76bfeb5ad72355e59312a38e4261c5841bf
GabrielDornianu <gabrieldornianu@yahoo.com>
2019-05-29 21:29:43 +0300

    Fixed port for heroky deployment again 1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,13 +3,15 @@ const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
 const message2 = document.querySelector('#message-2')
 
+const port = process.env.PORT || 3000
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
     message1.textContent = 'Loading...'
     message2.textContent = ''
 
-    fetch(`http://localhost:3000/weather?search=${location}`).then((response) => {
+    fetch(`http://localhost:${port}/weather?search=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 message1.textContent = data.error
